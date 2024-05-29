3edb80127cae99c4a6a975d013cefec423e84a08
smano-20052002 <smano4570@gmail.com>
2024-05-29 11:02:50 +0530

    Initial

---
+++ src/middleware/Course/AddContentMiddleware.jsx
@@ -0,0 +1,37 @@
+import { CREATE_CONTENT_REQUEST, SET_CONTENT_STATUS, createContentSuccess, createContentFailure, createContentExists } from "../../action/Course/AddContentAction";
+import axios from "axios";
+const API_URL = 'http://localhost:5199/lxp/course/material';
+
+const addContent = ({ dispatch, getState }) => (next) => async (action) => {
+    if (action.type === CREATE_CONTENT_REQUEST) {
+        console.log("isRequesting", getState().addContent);
+        const ReducerContentData = getState().addContent;
+        if (!ReducerContentData.isRequesting) {
+            dispatch({ type: SET_CONTENT_STATUS, payload: true });
+        
+        try {
+            console.log("Content posting data : ", action.payload);
+            const response = await axios.post(API_URL, action.payload, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+            if (response.data.statusCode == 412) {
+                dispatch(createContentExists());
+            }
+            else {
+                dispatch(createContentSuccess(response.data));
+                return next(action);
+             } }
+        catch (error) {
+            console.error('API Error:', error.message)
+            dispatch(createContentFailure(error.message))
+        } finally {
+            dispatch({ type: SET_CONTENT_STATUS, payload: false })
+        }
+    }
+    }
+    return next(action);
+};
+
+export default addContent;
